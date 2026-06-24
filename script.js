f4e0c2eaf8a7f0d78e461a049c1702c5cbae68cc
Захар <zsavvateev75@gmail.com>
2026-06-24 13:02:06 +0900

    Flexbox-вёрстка + подсветка меню

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 console.log("Привет! Скрипт подключен и работает.");
 
-const dateSpan = document.getElementById("update-date");
-const today = new Date();
+document.getElementById("update-date").textContent =
+    new Date().toLocaleDateString();
 
-dateSpan.textContent = today.toLocaleDateString("ru-RU");
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.forEach(l => l.classList.remove("active"));
+        link.classList.add("active");
+    });
+});
