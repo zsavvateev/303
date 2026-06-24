736b3c57b6fdb59006aab185a1fb11dd33b27a39
Захар <zsavvateev75@gmail.com>
2026-06-24 13:29:04 +0900

    Grid-сетка + адаптивность + бургер-меню

--- script.js
+++ script.js
@@ -7,7 +7,19 @@ const navLinks = document.querySelectorAll("nav a");
 
 navLinks.forEach(link => {
     link.addEventListener("click", () => {
-        navLinks.forEach(l => l.classList.remove("active"));
+        navLinks.forEach(l =>
+            l.classList.remove("active"));
+
         link.classList.add("active");
     });
+});
+
+const burgerBtn =
+    document.getElementById("burger-btn");
+
+const nav =
+    document.querySelector("nav");
+
+burgerBtn.addEventListener("click", () => {
+    nav.classList.toggle("open");
 });
